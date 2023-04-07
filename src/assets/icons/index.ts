d2bce0417af5b931366a8537ac3cdd915a8980ee
juju1321 <venera_2022@mail.ru>
2023-04-07 10:30:45 +0300

    add EmptyState component

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,4 @@
 export { HeartIcon } from "./HeartIcon";
 export { CancelIcon } from "./CancelIcon";
 export { ActiveHeartIcon } from "./ActiveHeartIcon";
+export { NoContentIcon } from "./NoContentIcon";
