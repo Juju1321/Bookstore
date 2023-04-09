b8d90dae38fd523a394220eb5b8e0e9677d8853e
juju1321 <venera_2022@mail.ru>
2023-04-09 19:26:11 +0300

    add unfinished SignIn page

---
+++ src/pages/SignIn/SignIn.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Button from "src/components/Button";
+import { ButtonType } from "src/utils/@globalTypes";
+
+const SignIn = () => {
+  return <div></div>;
+};
+
+export default SignIn;
