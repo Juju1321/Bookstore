ac153084bf9f9e3093c3a4b956456ffeba5d9bc4
juju1321 <venera_2022@mail.ru>
2023-05-02 16:41:09 +0300

    add reset Password and set new Password

--- src/pages/NewPassword/NewPassword.tsx
+++ src/pages/NewPassword/NewPassword.tsx
@@ -1,38 +1,83 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import AuthContainer from "src/pages/AuthContainer";
 import styles from "./NewPassword.module.scss";
 import Input from "src/components/Input";
 import Button from "src/components/Button";
-import {ButtonType} from "src/utils/@globalTypes";
+import { ButtonType } from "src/utils/@globalTypes";
+import { getAuth, confirmPasswordReset } from "firebase/auth";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { RoutesList } from "../Router";
 
 const NewPassword = () => {
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    const onChangePassword = (value: string) => setPassword(value);
-    const onChangeConfirmPassword = (value: string) => setConfirmPassword(value);
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const onChangePassword = (value: string) => setPassword(value);
+  const onChangeConfirmPassword = (value: string) => setConfirmPassword(value);
+  const [passwordError, setPasswordError] = useState("");
+  const [passwordTouched, setPasswordTouched] = useState(false);
 
-  return <AuthContainer>
+  useEffect(() => {
+    if (passwordTouched) {
+      if (password !== confirmPassword) {
+        setPasswordError("Passwords must match");
+      } else if (password.length === 0 || confirmPassword.length === 0) {
+        setPasswordError("Password is required field");
+      } else {
+        setPasswordError("");
+      }
+    }
+  }, [confirmPassword, password, passwordTouched]);
+
+  const onBlurPassword = () => {
+    setPasswordTouched(true);
+  };
+
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+
+  const oobCode = searchParams.get("oobCode");
+
+  const onNewPasswordClick =
+    (oobCode: string | null, newPassword: string) => () => {
+      const auth = getAuth();
+      if (oobCode)
+        confirmPasswordReset(auth, oobCode, newPassword)
+          .then(() => navigate(RoutesList.Auth))
+          .catch(() => alert("Invalid password"));
+    };
+
+  return (
+    <AuthContainer>
       <div className={styles.title}>new password</div>
       <div className={styles.infoContainer}>
-          <div className={styles.inputContainer}>
+        <div className={styles.inputContainer}>
           <Input
-              title={"Password"}
-              value={password}
-              onChange={onChangePassword}
-              placeholder={"Your password"}
-              type={"password"}
+            title={"Password"}
+            value={password}
+            onChange={onChangePassword}
+            placeholder={"Your password"}
+            type={"password"}
+            errorText={passwordError}
+            onBlur={onBlurPassword}
           />
           <Input
-              title={"Confirm password"}
-              value={confirmPassword}
-              onChange={onChangeConfirmPassword}
-              placeholder={"Confirm your password"}
-              type={"password"}
+            title={"Confirm password"}
+            value={confirmPassword}
+            onChange={onChangeConfirmPassword}
+            placeholder={"Confirm your password"}
+            type={"password"}
+            errorText={passwordError}
+            onBlur={onBlurPassword}
           />
-          </div>
-          <Button title={"set password"} onClick={() => {}} type={ButtonType.Primary} />
+        </div>
+        <Button
+          title={"set password"}
+          onClick={onNewPasswordClick(oobCode, password)}
+          type={ButtonType.Primary}
+        />
       </div>
-  </AuthContainer>;
+    </AuthContainer>
+  );
 };
 
 export default NewPassword;
--- src/pages/ResetPassword/ResetPassword.tsx
+++ src/pages/ResetPassword/ResetPassword.tsx
@@ -4,12 +4,27 @@ import Input from "src/components/Input";
 import Button from "src/components/Button";
 import styles from "./ResetPassword.module.scss";
 import { ButtonType } from "src/utils/@globalTypes";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { RoutesList } from "../Router";
+import { useNavigate } from "react-router-dom";
 
 const ResetPassword = () => {
   const [email, setEmail] = useState("");
   const [emailError, setEmailError] = useState("");
   const [emailTouched, setEmailTouched] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
   const onChangeEmail = (value: string) => setEmail(value);
+
+  const navigate = useNavigate();
+
+  const onResetClick = (email: string) => () => {
+    const auth = getAuth();
+    sendPasswordResetEmail(auth, email, { url: "http://localhost:3000/auth" })
+      .then(() => setIsVisible(true))
+      .catch(() => alert("Invalid user"));
+  };
+
+  const onGoToMainClick = () => navigate(RoutesList.Main);
 
   useEffect(() => {
     if (email.length === 0 && emailTouched) {
@@ -31,6 +46,13 @@ const ResetPassword = () => {
     <AuthContainer>
       <div className={styles.title}>reset password</div>
       <div className={styles.infoContainer}>
+        {isVisible && (
+          <div className={styles.receiveEmail}>
+            You will receive an email{" "}
+            <span className={styles.currentEmail}>{email}</span> with a link to
+            reset your password!
+          </div>
+        )}
         <Input
           title={"Email"}
           value={email}
@@ -40,12 +62,21 @@ const ResetPassword = () => {
           errorText={emailError}
           onBlur={onBlurEmail}
         />
-        <Button
-          title={"reset"}
-          disabled={!isValid}
-          onClick={() => {}}
-          type={ButtonType.Primary}
-        />
+        {isVisible ? (
+          <Button
+            title={"go to home"}
+            disabled={!isValid}
+            onClick={onGoToMainClick}
+            type={ButtonType.Primary}
+          />
+        ) : (
+          <Button
+            title={"reset"}
+            disabled={!isValid}
+            onClick={onResetClick(email)}
+            type={ButtonType.Primary}
+          />
+        )}
       </div>
     </AuthContainer>
   );
