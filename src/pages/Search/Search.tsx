3dde89575bfb2e2113ffc5832fad7bbb6634add5
juju1321 <venera_2022@mail.ru>
2023-05-06 19:54:42 +0300

    add tablet and mobile versions to the Search page

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import ReactPaginate from "react-paginate";
+import { useMediaQuery } from "react-responsive";
 import classNames from "classnames";
 
 import Title from "src/components/Title";
+import Input from "src/components/Input";
 import SearchList from "src/components/SearchList";
 import Loader from "src/components/Loader";
 import EmptyState from "src/components/EmptyState";
 import { PostSelector, getSearchedPosts } from "src/redux/reducers/postSlice";
 import styles from "./Search.module.scss";
+import { CancelIcon } from "src/assets/icons";
+import { RoutesList } from "../Router";
 
 const Search = () => {
   const searchValue = useSelector(PostSelector.getSearchValue);
@@ -17,8 +22,13 @@ const Search = () => {
   const isLoading = useSelector(PostSelector.getLoading);
 
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1149 });
+  const isMobile = useMediaQuery({ minWidth: 320, maxWidth: 767 });
 
   const [currentPage, setCurrentPage] = useState(1);
+  const [searchInputValue, setSearchValue] = useState("");
 
   const pagesCount = Math.ceil(postsCount / 10);
 
@@ -31,15 +41,43 @@ const Search = () => {
     setCurrentPage(selected + 1);
   };
 
+  const onHandleChange = (value: string) => setSearchValue(value);
+  const onCancelInput = () => navigate(RoutesList.Main)
+
+
   return searchList.length > 0 ? (
     <div>
+      {isTablet && <div className={styles.inputWithSearchButton}>
+          <Input
+            value={searchValue}
+            onChange={onHandleChange}
+            placeholder={"Search"}
+            type={"text"}
+            className={styles.input}
+          />
+          <div className={styles.searchIcon} onClick={onCancelInput}>
+            <CancelIcon />
+          </div>
+        </div>}
+        {isMobile && <div className={styles.inputWithSearchButton}>
+          <Input
+            value={searchValue}
+            onChange={onHandleChange}
+            placeholder={"Search"}
+            type={"text"}
+            className={styles.input}
+          />
+          <div className={styles.searchIcon} onClick={onCancelInput}>
+            <CancelIcon />
+          </div>
+        </div>}
       <Title
         title={`"${searchValue}" search results`}
         className={styles.title}
       />
-      <div className={styles.description}>{`Found ${postsCount} books`}</div>
+      {!isTablet && !isMobile && <div className={styles.description}>{`Found ${postsCount} books`}</div>}
       {isLoading ? <Loader /> : <SearchList cardList={searchList} />}
-      <ReactPaginate
+      {!isMobile && <ReactPaginate
         pageCount={pagesCount}
         forcePage={currentPage - 1}
         onPageChange={onPageChange}
@@ -58,7 +96,7 @@ const Search = () => {
         })}
         previousLinkClassName={styles.linkPage}
         nextLinkClassName={styles.linkPage}
-      />
+      />}
     </div>
   ) : (
     <EmptyState
