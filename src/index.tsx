74168773d3c9397b1a33c66969c9970dc63c2821
juju1321 <venera_2022@mail.ru>
2023-05-07 17:18:15 +0300

    add redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App";
-import store from "src/redux/store";
+import store, { persistor } from "src/redux/store";
 import "./index.css";
 import "./firebase";
 
@@ -12,6 +13,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <App />
+    </PersistGate>
   </Provider>
 );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,7 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import postReducer from "./reducers/postSlice";
 import cartReducer from "./reducers/cartSlice";
@@ -8,17 +10,27 @@ import rootSaga from "src/redux/sagas/rootSaga";
 
 const sagaMiddleware = createSagaMiddleware();
 
+const rootReducer = combineReducers({
+  posts: postReducer,
+  cart: cartReducer,
+  user: userReducer,
+});
+
+const persostConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persostConfig, rootReducer);
+
 const store = configureStore({
-  reducer: {
-    posts: postReducer,
-    cart: cartReducer,
-    user: userReducer,
-  },
+  reducer: persistedReducer,
   middleware: [sagaMiddleware]
 });
 
 sagaMiddleware.run(rootSaga)
 
 export type RootState = ReturnType<typeof store.getState>;
+export let persistor = persistStore(store);
 
 export default store;
